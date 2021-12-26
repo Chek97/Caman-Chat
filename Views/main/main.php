91d53b1a4bfdbe1d24a4cd25ee0067eb1eb0461d
Chek97 <felychk203@hotmail.com>
2021-12-25 21:19:34 -0500

    agregados estilos e correcion del contacto borrado

--- Views/main/main.php
+++ Views/main/main.php
@@ -17,11 +17,13 @@
             require_once('./contactsList.php');
         ?>
         <section class="main-conversations">
-            Conversaciones pendientes
-            <!-- Ajustar el tamaño con grid css -->
-            <div class="main-header">
+            <div class="main-pendient">
+                <p>Conversaciones pendientes</p>
+            </div>
+            <div class="main-user-edit">
                 <p>
-                    Bienvenido Usuario <strong><?php echo($_SESSION['user']['username'])?></strong> <a href="./editUser.php?id=<?php echo($_SESSION['user']['id']); ?>">Editar</a><!-- EDITAR USUARIO -->
+                    Bienvenido Usuario <strong><?php echo($_SESSION['user']['username'])?></strong> 
+                    <a href="./editUser.php?id=<?php echo($_SESSION['user']['id']); ?>">Editar <i class="fa fa-pencil" aria-hidden="true"></i></a>
                 </p>
             </div>
         </section>
